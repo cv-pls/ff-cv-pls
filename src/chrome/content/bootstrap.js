b6a4d6fbbf1314fa42f1bafacd1b4784249ea40e
Chris Wright <chrisw@networkm.co.uk>
2012-12-05 02:29:45 +0000

    Brought submodule up to date

--- src/chrome/content/bootstrap.js
+++ src/chrome/content/bootstrap.js
@@ -1,46 +1,52 @@
 /*jslint plusplus: true, white: true, browser: true */
 /*global CvPlsHelper */
 
-CvPlsHelper.firefox.Bootstrap = {
+(function() {
 
-  onPageLoad: function(e) {
+  'use strict';
 
-    'use strict';
+  CvPlsHelper.firefox.Bootstrap = {
+    onPageLoad: function(e) {
+      var application, opts;
 
-    var application, opts;
+      if (/^https?:\/\/chat\.stackoverflow\.com\/(rooms|transcript)\//i.test(e.originalTarget.location.href)) {
 
-    if (/^https?:\/\/chat\.stackoverflow\.com\/(rooms|transcript)\//i.test(e.originalTarget.location.href)) {
-      application = new CvPlsHelper.ChatApplication(e.originalTarget);
-    } else if (/^https?:\/\/stackoverflow\.com\/questions\//i.test(e.originalTarget.location.href)) {
-      opts = {
-        SettingsDataAccessor: CvPlsHelper.firefox.SettingsDataAccessor,
-        SettingsDataStore: CvPlsHelper.firefox.SettingsDataStore,
-        DefaultSettings: CvPlsHelper.firefox.DefaultSettings,
-        DesktopNotificationDispatcher: CvPlsHelper.firefox.DesktopNotificationDispatcher
-      };
-      application = new CvPlsHelper.QuestionApplication(e.originalTarget, opts, function(){});
+        opts = {
+          SettingsDataAccessor: CvPlsHelper.firefox.SettingsDataAccessor,
+          SettingsDataStore: CvPlsHelper.firefox.SettingsDataStore,
+          DefaultSettings: CvPlsHelper.firefox.DefaultSettings,
+          DesktopNotificationDispatcher: CvPlsHelper.firefox.DesktopNotificationDispatcher
+        };
+        application = new CvPlsHelper.ChatApplication(e.originalTarget, opts, function(){});
+
+      } else if (/^https?:\/\/stackoverflow\.com\/questions\//i.test(e.originalTarget.location.href)) {
+        /*
+        opts = {
+          SettingsDataAccessor: CvPlsHelper.firefox.SettingsDataAccessor,
+          SettingsDataStore: CvPlsHelper.firefox.SettingsDataStore,
+          DefaultSettings: CvPlsHelper.firefox.DefaultSettings,
+          DesktopNotificationDispatcher: CvPlsHelper.firefox.DesktopNotificationDispatcher
+        };
+        application = new CvPlsHelper.QuestionApplication(e.originalTarget, opts, function(){});
+        */
+      }
+
+      if (application !== undefined) {
+        e.originalTarget.defaultView.addEventListener('unload', function() {
+          application.shutdown();
+          application = null;
+        }, true);
+        application.start();
+      }
+    },
+
+    onWindowLoad: function() {
+      window.removeEventListener('load', CvPlsHelper.firefox.Bootstrap.onWindowLoad); // Should only run once
+      CvPlsHelper.content = document.getElementById('appcontent');
+      CvPlsHelper.content.addEventListener('DOMContentLoaded', CvPlsHelper.firefox.Bootstrap.onPageLoad, true);
     }
+  };
 
-    if (application !== undefined) {
-      e.originalTarget.defaultView.addEventListener('unload', function() {
-        application.shutdown();
-        delete application;
-      }, true);
-      application.start();
-    }
+  window.addEventListener('load', CvPlsHelper.firefox.Bootstrap.onWindowLoad);
 
-  },
-
-  onWindowLoad: function() {
-
-    'use strict';
-
-    window.removeEventListener('load', CvPlsHelper.firefox.Bootstrap.onWindowLoad); // Should only run once
-    CvPlsHelper.content = document.getElementById('appcontent');
-    CvPlsHelper.content.addEventListener('DOMContentLoaded', CvPlsHelper.firefox.Bootstrap.onPageLoad, true);
-
-  }
-
-};
-
-window.addEventListener('load', CvPlsHelper.firefox.Bootstrap.onWindowLoad);
+}());
