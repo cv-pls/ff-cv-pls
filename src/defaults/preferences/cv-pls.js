1bcd35b6d37941566a0ea5cd56607b5991ccc7f8
Chris Wright <chrisw@networkm.co.uk>
2013-01-02 01:10:28 +0000

    Moved default settings to core

--- src/defaults/preferences/cv-pls.js
+++ src/defaults/preferences/cv-pls.js
@@ -2,24 +2,24 @@
 // Oneboxing
 pref('extensions.cv-pls.oneBox', true);
 pref('extensions.cv-pls.oneBoxHeight', 30);
-pref('extensions.cv-pls.removeCompletedOneboxes', false);
+pref('extensions.cv-pls.removeCompletedOneboxes', true);
 
 // Extra buttons in chat
 pref('extensions.cv-pls.cvPlsButton', true);
-pref('extensions.cv-pls.delvPlsButton', false);
+pref('extensions.cv-pls.delvPlsButton', true);
 
 // Vote request notification
 pref('extensions.cv-pls.soundNotification', false);
-pref('extensions.cv-pls.avatarNotification', false);
-pref('extensions.cv-pls.removeLostNotifications', false);
-pref('extensions.cv-pls.removeCompletedNotifications', false);
+pref('extensions.cv-pls.avatarNotification', true);
+pref('extensions.cv-pls.removeLostNotifications', true);
+pref('extensions.cv-pls.removeCompletedNotifications', true);
 pref('extensions.cv-pls.desktopNotification', false);
 
 // Vote request status
 pref('extensions.cv-pls.showCloseStatus', true);
-pref('extensions.cv-pls.pollCloseStatus', false);
+pref('extensions.cv-pls.pollCloseStatus', true);
 pref('extensions.cv-pls.pollInterval', 5);
-pref('extensions.cv-pls.strikethroughCompleted', false);
+pref('extensions.cv-pls.strikethroughCompleted', true);
 
 // Backlog
 pref('extensions.cv-pls.backlogEnabled', false);
